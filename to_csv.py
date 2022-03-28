709463a7b147a479f8d7f1255a96ee7700a1f8c7
Arnav Zutshi <arnzut1324@gmail.com>
2022-03-28 12:36:27 +0530

    Flask API and CSV

---
+++ to_csv.py
@@ -0,0 +1,17 @@
+from csv import writer
+  
+# The data assigned to the list 
+list_data=['4.5','2.5','8.9']
+  
+# Pre-requisite - The CSV file should be manually closed before running this code.
+
+# First, open the old CSV file in append mode, hence mentioned as 'a'
+# Then, for the CSV file, create a file object
+with open('test1.csv', 'a', newline='') as f_object:  
+    # Pass the CSV  file object to the writer() function
+    writer_object = writer(f_object)
+    # Result - a writer object
+    # Pass the data in the list as an argument into the writerow() function
+    writer_object.writerow(list_data)  
+    # Close the file object
+    f_object.close()
